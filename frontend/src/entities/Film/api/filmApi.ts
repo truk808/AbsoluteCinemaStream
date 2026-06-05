e059f10b924675a7a958d4dd94a29ad3d63e7ae4
Илья <ilay.shulga.ru@gmail.com>
2026-06-05 19:03:42 +0300

    loading

--- frontend/src/entities/Film/api/filmApi.ts
+++ frontend/src/entities/Film/api/filmApi.ts
@@ -2,10 +2,12 @@ import {$kinopoiskHost} from "../../../shared/api";
 
 export const getFilmById = async (id: number) => {
     const { data } = await $kinopoiskHost.get(`/api/v2.2/films/${id}`);
+    console.log('getFilmById', data)
     return data;
 }
 
 export const getFilmTrailerById = async (id: number) => {
     const { data } = await $kinopoiskHost.get(`/api/v2.2/films/${id}/videos`);
+    console.log('getFilmTrailerById', data)
     return data;
 }
--- frontend/src/widjets/HeroMovieInfo/ui/HeroMovieInfo.tsx
+++ frontend/src/widjets/HeroMovieInfo/ui/HeroMovieInfo.tsx
@@ -2,11 +2,17 @@ import {OpenTrailer} from "../../../features/OpenTrailer";
 import {useHeroMovieInfo} from "../model/useHeroMovieInfo.ts";
 import {RateFilm} from "../../../features/RateFilm";
 import {AddToWatchList} from "../../../features/AddToWatchList";
+import {useEffect} from "react";
 
 export const HeroMovieInfo = () => {
-    const {film, trailer} = useHeroMovieInfo()
+    const {film, trailer, isLoading, error} = useHeroMovieInfo()
 
-    if (!film) return <div className='w-full h-full text-white text-9xl felx justify-center items-center'>загрузка</div>;
+    if (!film) return <div className='w-full h-full text-white text-9xl felx justify-center items-center'>фильма нет! :(</div>;
+
+    if (isLoading) return <svg class="size-8 animate-spin text-brand-text" xmlns="http://w3.org" fill="none" viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>
 
     return (
         <div className=" min-h-screen bg-brand-bg overflow-x-hidden">
